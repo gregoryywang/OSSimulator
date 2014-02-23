be6c16563ca368046f69291293ed1ee08a3e9f63
yongyuwang5@gmail.com <yongyuwang5@gmail.com@d75594fe-b918-0e31-e242-1b4de4dba884>
2014-02-23 04:17:29 +0000

    Created IODevices class

---
+++ trunk/ ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/IODevice.java
@@ -0,0 +1,21 @@
+
+package edu.uw.tcss422;
+
+public class IODevice {
+	
+	public void runIO() {
+		while (true) {
+			keyboard();
+			disk();
+		}
+	}
+	
+	public void keyboard() {
+		
+	}
+	
+	public void disk() {
+		
+	}
+	
+}
