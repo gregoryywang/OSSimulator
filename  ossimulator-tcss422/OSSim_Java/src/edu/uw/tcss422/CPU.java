5ab4d3d8bc4fb3f3194f24ce25a5221bdf0eef43
cmptrz@uw.edu <cmptrz@uw.edu>
2014-02-25 23:46:19 +0000

    Added scanner with prompts to determine how many processes to create and which scheduling policy to use.

---  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/CPU.java
+++  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/CPU.java
@@ -1,12 +1,28 @@
 package edu.uw.tcss422;
 
+import java.util.Scanner;
+
 public class CPU {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hi!");
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the number of UI processes you would like: ");
+		int UIprocesses = scan.nextInt();
+		System.out.print("Enter the number of calculating processes you would like: ");
+		int calculatingProcesses = scan.nextInt();
+		System.out.print("Enter the number of producer/consumer pairs you would like: ");
+		int ProdConsumProcesses = scan.nextInt();
+		System.out.print("Enter the number of I/O processes you would like: ");
+		int IOprocesses = scan.nextInt();
+		int schedulerPolicy = 0;
+		do {
+			System.out.print("Please indicate which scheduling policy (1 for round-robin, 2 for priority, 3 for lottery): ");
+			schedulerPolicy = scan.nextInt();
+		} while (schedulerPolicy < 1 || schedulerPolicy > 3);
+		scan.close();
 	}
 
 }
