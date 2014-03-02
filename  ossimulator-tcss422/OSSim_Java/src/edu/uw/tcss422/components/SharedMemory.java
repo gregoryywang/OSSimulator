3be7ea03953a855b381653d90cf243235455d50f
cmptrz@uw.edu <cmptrz@uw.edu>
2014-03-02 02:51:43 +0000

    Added guts to SharedMemory.java

---  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/SharedMemory.java
+++  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/SharedMemory.java
@@ -1,28 +1,44 @@
 package edu.uw.tcss422.components;
 
-import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 
+/**
+ * The shared memory class which contains all of the memory used by the
+ * intercommunicating process pairs.
+ */
 public class SharedMemory {
 	
-	private ArrayList<ArrayBlockingQueue<Integer>> myMemory;
+	private ArrayBlockingQueue<Integer>[] myMemory;
 	
+	@SuppressWarnings("unchecked")
 	public SharedMemory(int capacity, int numberOfPairs) {
-		myMemory = new ArrayList<ArrayBlockingQueue<Integer>>(numberOfPairs);
+		myMemory = (ArrayBlockingQueue<Integer>[]) new ArrayBlockingQueue<?>[numberOfPairs];
+		
+		for (int i = 0; i < numberOfPairs; i++) {
+			myMemory[i] = new ArrayBlockingQueue<Integer>(capacity);
+		}
 	}
 	
-	public boolean increment(int index) {
-		boolean isSuccessful = false;
+	/*
+	 * Adds the value to the stack and returns a boolean.
+	 */
+	public boolean push(int index, int value) {
+		boolean isSuccessful;
 		
-		
+		isSuccessful = myMemory[index].offer(value);
 		
 		return isSuccessful;
 	}
 	
-	public boolean decrement(int index) {
-		boolean isSuccessful = false;
+	/*
+	 * Returns the head of the stack (or null of stack is empty).
+	 */
+	public int pop(int index) {
+		int output;
 		
-		return isSuccessful;
+		output = myMemory[index].poll();
+		
+		return output;
 	}
 
 }
