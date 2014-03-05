7186a713797a2da90ce8947bed78dbd542f5fa24
cmptrz@uw.edu <cmptrz@uw.edu@d75594fe-b918-0e31-e242-1b4de4dba884>
2014-03-05 18:39:06 +0000

    Implemented Round Robin and Lottery.

--- trunk/ ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/Scheduler.java
+++ trunk/ ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/Scheduler.java
@@ -1,5 +1,9 @@
 package edu.uw.tcss422.components;
 
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import edu.uw.tcss422.types.ProcessState;
 import edu.uw.tcss422.types.SchedulePolicy;
 
 public class Scheduler {
@@ -15,36 +19,51 @@ public class Scheduler {
 			currentSchedule = SchedulePolicy.LOTTERY;
 	}
 	
-	public long getNextProcessID() {
+	public long getNextProcessID(PCBList currentList) {
 		
 		switch(currentSchedule) {
 			case ROUND_ROBIN:
-				return roundRobin();
+				return roundRobin(currentList);
 				
 			case PRIORITY:
-				return priority();
+				return priority(currentList);
 				
 			case LOTTERY:
-				return lottery();
+				return lottery(currentList);
 				
 			default:
 				return 0;
 		}
 	}
 
-	private long roundRobin() {
+	private long roundRobin(PCBList currentList) {
+		int currentPID = 0;	// Should maybe begin at 1?
+		do {
+			currentPID++;
+		} while (currentList.getPCBList().get(currentPID).getState() == ProcessState.RUNNING);
+		
+		int nextPID = currentPID++;
+		
+		while (currentList.getPCBList().get(nextPID).getState() != ProcessState.BLOCKED) {
+			nextPID = (nextPID++) % currentList.getPCBList().size();
+		}
+		
+		return nextPID;
+	}
+
+	private long priority(PCBList currentList) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	private long priority() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	private long lottery() {
-		// TODO Auto-generated method stub
-		return 0;
+	private long lottery(PCBList currentList) {
+		int size = currentList.getPCBList().size();
+		ProcessControlBlock pcb;
+		do {
+			pcb = currentList.getPCBList().get((int) (Math.random() * size));
+		} while (pcb.getState() != ProcessState.BLOCKED);
+		
+		return pcb.getPid();
 	}
 
 }
