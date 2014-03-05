1ce972e1dd97f2bae2cc1031d9ef215566ac1dae
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2014-03-05 21:36:34 +0000

    Added timing logic.

---  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/SystemTimer.java
+++  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/SystemTimer.java
@@ -1,11 +1,36 @@
 package edu.uw.tcss422.components;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class SystemTimer {
-	public SystemTimer(){
-		start();
-	}
-	public void start(){
-		// generates interrupts
+	
+	private boolean debugFlag = false;
+
+	public SystemTimer(int min, int max, boolean debugFlag) {
+		
+		this.debugFlag = debugFlag;
+		
+		Random random = new Random();
+		long delay = random.nextInt((max - min) + 1) + min;
+		
+		Timer timer = new Timer();
+		timer.schedule(new GenerateInterrupts(), Long.valueOf(delay));
+		
+		if(debugFlag) {
+			System.out.println("System timer delay set to " + delay + " milliseconds.");
+		}
+
 	}
 	
+	class GenerateInterrupts extends TimerTask {
+		public void run() {
+			// calls interrupt method in CPU
+			if(debugFlag) {
+				System.out.println("Timer has triggered a CPU interrupt.");
+			}
+		}
+	}
 }
