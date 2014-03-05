fff7f0b46ee869c03ea7a229ca1231d242e90720
oscar01200012@gmail.com <oscar01200012@gmail.com@d75594fe-b918-0e31-e242-1b4de4dba884>
2014-03-05 03:24:37 +0000

    Add documentation for ProcessControlBlock.

--- trunk/ ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/ProcessControlBlock.java
+++ trunk/ ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/ProcessControlBlock.java
@@ -4,52 +4,108 @@ import edu.uw.tcss422.types.ProcessState;
 
 public class ProcessControlBlock {
 
+	/**
+	 * The process ID.
+	 */
 	private int pid;
 	
+	/**
+	 * This is the step count that the CPU had gotten to when this process was preempted. (Like a PC register value).
+	 */
 	private int nextStep;
 	
+	/**
+	 * The state of the process.
+	 */
 	private ProcessState state;
 	
+	/**
+	 * The actual process.
+	 */
 	private Process process;
 	
+	/**
+	 * Which mutex lock does it own. Can be used for producer-consumer pair.
+	 */
 	private int mutex;
 	
-	public ProcessControlBlock(int pid, int nextStep, ProcessState state, Process process, int mutex) {
+	/**
+	 * Construct a Process Control Block. Initially the step count is 0 and the ProcessState is Ready.
+	 * @param pid the process ID
+	 * @param nextStep 
+	 * @param state
+	 * @param process
+	 * @param mutex
+	 */
+	public ProcessControlBlock(int pid, Process process, int mutex) {
 		this.pid = pid;
-		this.nextStep = nextStep;
-		this.state = state;
+		this.nextStep = 0;
+		this.state = ProcessState.READY;
 		this.process = process;
 		this.mutex = mutex;
 	}
 	
+	/**
+	 * Get the Process ID (pid).
+	 * @return the process ID (pid)
+	 */
 	public int getPid() {
 		return pid;
 	}
 	
+	/**
+	 * Get the next step. <br>
+	 * This is the step count that the CPU had gotten to when this process was preempted. (Like a PC register value).
+	 * @return the next step
+	 */
 	public int getNextStep() {
 		return nextStep;
 	}
 	
+	/**
+	 * Get the state of the process.
+	 * @return the state of the process
+	 */
 	public ProcessState getState() {
 		return state;
 	}
 	
+	/**
+	 * Get the actual process
+	 * @return the actual process
+	 */
 	public Process getProcess() {
 		return process;
 	}
 	
+	/**
+	 * Get the mutex lock that this process own.
+	 * @return the mutex lock that this process own
+	 */
 	public int getMutex() {
 		return mutex;
 	}
 	
+	/**
+	 * Set the next step 
+	 * @param nextStep the next step
+	 */
 	public void setNextStep(int nextStep) {
 		this.nextStep = nextStep;
 	}
 	
+	/**
+	 * Set the process state
+	 * @param state the process state
+	 */
 	public void setState(ProcessState state) {
 		this.state = state;
 	}
 	
+	/**
+	 * Set a mutex.
+	 * @param mutex a mutex
+	 */
 	public void setMutex(int mutex) {
 		this.mutex = mutex;
 	}
