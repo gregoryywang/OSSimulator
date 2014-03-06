0d8a0966d7df3e9ff400c8dfd6150b483f089f3b
rjonesca97@gmail.com <rjonesca97@gmail.com>
2014-03-06 16:12:47 +0000

    Added code for CPU loop

---  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/CPU.java
+++  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/CPU.java
@@ -1,6 +1,5 @@
 package edu.uw.tcss422.components;
 
-import process.IdleProcess;
 import edu.uw.tcss422.types.GenericProcess;
 import edu.uw.tcss422.types.ProcessType;
 
@@ -21,29 +20,67 @@ public class CPU extends Thread {
    */
   private Scheduler scheduler;
   
+  /**
+   * Kill condition variable.
+   */
+  private boolean bKill = false;
+  
   public CPU(PCBList pcbList, SharedMemory sharedMemory, Scheduler scheduler) {
     this.pcbList = pcbList;
     this.sharedMemory = sharedMemory;
     this.scheduler = scheduler;
   }
   
+  /**
+   * Sets the kill condition variable of the CPU.
+   */
+  public synchronized void kill() {
+	 bKill = true;
+  }
+  
   @Override
   public void run() {
-    int triggerPoint;
-    
-    while(true) {
-      
-      
-    }
+	  int triggerPoint = 0;
+	  int PC = 0;
+
+	  while(bKill) {
+		  //Get next process to run
+		  int PID = (int) scheduler.getNextProcessID(pcbList); //Temp until scheduler interface changes
+
+		  //Retrieve PCB for current process
+		  ProcessControlBlock pcb = pcbList.getPCB(PID);
+
+		  //Get trigger point
+		  triggerPoint = pcb.getProcess().getTriggerPoint();
+
+		  //Get current PC
+		  PC = pcb.getNextStep();
+
+		  //Get process type
+		  ProcessType type = pcb.getProcess().getProcessType();
+
+		  //Loop through all instructions and roll over when max instructions has been met.
+		  while(triggerPoint != PC )
+			  PC = (PC + 1) % GenericProcess.MAX_INSTRUCTIONS;
+
+		  //Make system call based on process type
+		  systemCall(type);
+	  }
 
   }
   
+  /**
+   * Helper method that dispatches system calls based on process type.
+   * In our simulation, there is a one to one mapping of system call to process type.
+   * @param type The process type of the requesting process.
+   */
   private void systemCall(ProcessType type) {
     
     if( type == ProcessType.COMPUTE ) {
       
     } else if( type == ProcessType.CONSUMER ) {
-      
+    	//Example
+    	updateSharedMemory();
     } else if( type == ProcessType.PRODUCER ) {
       
     } else if( type == ProcessType.IDLE ) {
@@ -51,5 +88,13 @@ public class CPU extends Thread {
     } else if( type == ProcessType.UI ) {
       
     }
+  }
+  
+  //Example of a system call
+  /**
+   * The actual system call method.
+   */
+  private void updateSharedMemory() {
+	  
   }
 }
