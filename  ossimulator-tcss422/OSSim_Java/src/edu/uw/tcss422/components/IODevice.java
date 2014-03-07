624e2caefe4d66884ae8cedcd178588b481d450b
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2014-03-07 20:54:08 +0000

    Implemented IODevice. Warning: Untested!

---  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/IODevice.java
+++  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/IODevice.java
@@ -1,21 +1,45 @@
 
 package edu.uw.tcss422.components;
 
-public class IODevice {
+import java.util.Random;
+
+public class IODevice implements Runnable {
 	
-	public void runIO() {
-		while (true) {
-			keyboard();
-			disk();
+	private CPU currentCPU;
+	
+	private String deviceType;
+	
+	private int max, min;
+	
+	private boolean debugFlag = false;
+	
+	public IODevice(CPU currentCPU, String deviceType, int max, int min) {
+		this.currentCPU = currentCPU;
+		this.deviceType = deviceType;
+		this.max = max;
+		this.min = min;
+	}
+
+	@Override
+	public void run() {
+		Random random = new Random();
+		while(true) {
+			int delayPeriod = random.nextInt((max - min) + 1) + min;
+			try {
+				Thread.sleep(Long.valueOf(delayPeriod));
+			} catch (InterruptedException e) {
+				System.out.println("IODevices thread interrupted! This is not supposed to happen.");
+				e.printStackTrace();
+			}
+			
+			//TODO: call interrupt method in CPU.
+			
+			if(debugFlag = true) {
+				System.out.println("IODevice generated an interrupt. Delay time = " + delayPeriod +
+						" milliseconds");
+			}
+			
 		}
-	}
-	
-	public void keyboard() {
-		
-	}
-	
-	public void disk() {
-		
 	}
 	
 }
