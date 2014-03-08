86596d39bf7d6ee2986b6dc360c1571da2d45b1d
oscar01200012@gmail.com <oscar01200012@gmail.com>
2014-03-08 17:46:09 +0000

    Implement system calls and interupts.

---  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/CPU.java
+++  ossimulator-tcss422/OSSim_Java/src/edu/uw/tcss422/components/CPU.java
@@ -1,113 +1,147 @@
 package edu.uw.tcss422.components;
 
 import edu.uw.tcss422.types.GenericProcess;
+import edu.uw.tcss422.types.ProcessState;
 import edu.uw.tcss422.types.ProcessType;
 import edu.uw.tcss422.types.SchedulePolicy;
 
 public class CPU extends Thread {
-  
-  /**
-   * Reference to PCBList.
-   */
-  private PCBList pcbList;
-  
-  /**
-   * Reference Shared Memory.
-   */
-  private SharedMemory sharedMemory;
-  
-  /**
-   * Reference to Scheduler.
-   */
-  private Scheduler scheduler;
-  
-  /**
-   * Kill condition variable.
-   */
-  private boolean bKill = true;
-  
-  /**
-   * Interrupted condition variable.
-   */
-  private boolean bInterrupted = false;
-  
-  public CPU(PCBList pcbList, SharedMemory sharedMemory, Scheduler scheduler) {
-    this.pcbList = pcbList;
-    this.sharedMemory = sharedMemory;
-    this.scheduler = scheduler;
-  }
-  
-  /**
-   * Sets the kill condition variable of the CPU.
-   */
-  public synchronized void kill() {
-	 bKill = true;
-  }
-  
-  @Override
-  public void run() {
-	  int triggerPoint = 0;
-	  int PC = 0;
-		
-	  while (bKill) {
-		  
-		  //Get next process to run
-		  int PID = scheduler.getNextProcessID(); //Temp until scheduler interface changes
 
-		  //Retrieve PCB for current process
-		  ProcessControlBlock pcb = pcbList.getPCB(PID);
+	/**
+	 * Reference to PCBList.
+	 */
+	private PCBList pcbList;
 
-		  //Get trigger point
-		  triggerPoint = pcb.getProcess().getTriggerPoint();
+	/**
+	 * Reference Shared Memory.
+	 */
+	private SharedMemory sharedMemory;
 
-		  //Get current PC
-		  PC = pcb.getNextStep();
+	/**
+	 * Reference to Scheduler.
+	 */
+	private Scheduler scheduler;
 
-		  //Loop through all instructions
-		  while(PC != GenericProcess.MAX_INSTRUCTIONS - 1) {
-			
-			if(PC == triggerPoint) {
-				//Make system call based on process type
-				systemCall(PID);
-			} else if(scheduler.getCurrentSchedulerPolicy() == SchedulePolicy.LOTTERY 
-					|| scheduler.getCurrentSchedulerPolicy() == SchedulePolicy.PRIORITY
-					&& PC == GenericProcess.MAX_INSTRUCTIONS - 1){
-				break;
+	/**
+	 * Kill condition variable.
+	 */
+	private boolean bKill = true;
+
+	/**
+	 * Interrupted condition variable.
+	 */
+	private boolean bInterrupted = false;
+
+	public CPU(PCBList pcbList, SharedMemory sharedMemory, Scheduler scheduler) {
+		this.pcbList = pcbList;
+		this.sharedMemory = sharedMemory;
+		this.scheduler = scheduler;
+	}
+
+	/**
+	 * Sets the kill condition variable of the CPU.
+	 */
+	public synchronized void kill() {
+		bKill = true;
+	}
+
+	@Override
+	public void run() {
+		int triggerPoint = 0;
+		int PC = 0;
+
+		while (bKill) {
+
+			//Get next process to run
+			int PID = scheduler.getNextProcessID(); //Temp until scheduler interface changes
+
+			//Retrieve PCB for current process
+			ProcessControlBlock pcb = pcbList.getPCB(PID);
+
+			//Get trigger point
+			triggerPoint = pcb.getProcess().getTriggerPoint();
+
+			//Get current PC
+			PC = pcb.getNextStep();
+
+			//Loop through all instructions
+			while(PC != GenericProcess.MAX_INSTRUCTIONS - 1) {
+
+				if (PC == triggerPoint) {
+					//Make system call based on process type
+					systemCall(PID);
+				} else if (bInterrupted) {
+					// Perform action for hardware interupt
+					// Then change the state of a blocked UIProcess to READY
+					// Seems like the IODevice should have something that can be paired with a UIProcess 
+
+				} else if(scheduler.getCurrentSchedulerPolicy() == SchedulePolicy.LOTTERY 
+						|| scheduler.getCurrentSchedulerPolicy() == SchedulePolicy.PRIORITY
+						&& PC == GenericProcess.MAX_INSTRUCTIONS - 1){
+					break;
+				}
+
+				PC = (PC + 1) % (GenericProcess.MAX_INSTRUCTIONS - 1);
 			}
-			
-			PC = (PC + 1) % (GenericProcess.MAX_INSTRUCTIONS - 1);
-		  }
-	  }
-  }
-  
-  /**
-   * Helper method that dispatches system calls based on process type.
-   * In our simulation, there is a one to one mapping of system call to process type.
-   * @param type The process type of the requesting process.
-   */
-  private void systemCall(int PID) {
-    
-    ProcessType type = pcbList.getPCB(PID).getProcess().getProcessType();
-    
-    if( type == ProcessType.COMPUTE ) {
-      
-    } else if( type == ProcessType.CONSUMER ) {
-    	//Example
-    	updateSharedMemory();
-    } else if( type == ProcessType.PRODUCER ) {
-      
-    } else if( type == ProcessType.IDLE ) {
-      
-    } else if( type == ProcessType.UI ) {
-      
-    }
-  }
-  
-  //Example of a system call
-  /**
-   * The actual system call method.
-   */
-  private void updateSharedMemory() {
-	  
-  }
+		}
+	}
+
+	/**
+	 * This method will be called by IODevices.
+	 */
+	public void IOinterupt() {
+		bInterrupted = true;
+	}
+
+
+
+	/**
+	 * Helper method that dispatches system calls based on process type.
+	 * In our simulation, there is a one to one mapping of system call to process type.
+	 * @param type The process type of the requesting process.
+	 */
+	private void systemCall(int PID) {
+		ProcessControlBlock pcb = pcbList.getPCB(PID);
+
+		ProcessType type = pcb.getProcess().getProcessType();
+
+		if( type == ProcessType.COMPUTE ) {
+			// It does nothing
+		} else if( type == ProcessType.CONSUMER ) {
+			// request resource
+			requestResource(pcb);
+		} else if( type == ProcessType.PRODUCER ) {
+			// I think it always try to put stuff in the memory.
+			putResource(pcb);
+		} else if( type == ProcessType.IDLE ) {
+			// It does nothing
+		} else if( type == ProcessType.UI ) {
+			// Request resource from IODevice. Do we use the sharedMemory or other mechanism?
+			// I'm assuming that we are using the sharedMemory here
+			requestResource(pcb);
+		}
+	}
+
+	/**
+	 * The system call to request a resource from shared memory.
+	 */
+	private void requestResource(ProcessControlBlock pcb) {
+		if (sharedMemory.isEmpty(pcb.getMutex())) {
+			// resource not available
+			pcb.setState(ProcessState.BLOCKED);
+		} else {
+			// resource available
+			pcb.setState(ProcessState.RUNNING);
+			sharedMemory.pop(pcb.getMutex());
+		}
+	}
+
+	private void putResource(ProcessControlBlock pcb) {
+		int mutex = pcb.getMutex();
+		if (!sharedMemory.isEmpty(pcb.getMutex())) {
+			sharedMemory.push(mutex, 1); // 1 represents dummy data
+			// Seems like it has to update the consumer from BLOCKED to READY
+			pcbList.getPCBbyMutex(mutex).setState(ProcessState.READY);
+		}
+	}
 }
